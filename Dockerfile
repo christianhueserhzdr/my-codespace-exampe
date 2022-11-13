cf0d6c9b3b45f55a00e556c32f698a7c742fe81f
Christian Hueser <55481699+christianhueserhzdr@users.noreply.github.com>
2022-11-13 19:46:26 +0000

    use vscode user and set permissions

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,10 @@ ENV PYTHONDONTWRITEBYTECODE=1
 ENV PYTHONUNBUFFERED=1
 
 WORKDIR /code
+
 COPY . /code/
+
 RUN pip install poetry
 RUN poetry install
+
+RUN chown -R $USER:$USER my_codespace_example/hello_world my_codespace_example/manage.py
--- my_codespace_example/hello_world/settings.py
+++ my_codespace_example/hello_world/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-)m9cu5(pdok2y_&7e1pffnszcj#^cm7(#_eup=831rch4e4iiv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
